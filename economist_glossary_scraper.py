db2d1731212d7fab89469ce5e2ba04d9fbd08d87
Ahmed_Hany <singularity717@gmail.com>
2018-06-10 09:14:16 +0200

    Update economist_glossary_scraper.py

--- economist_glossary_scraper.py
+++ economist_glossary_scraper.py
@@ -6,13 +6,13 @@ from string import ascii_lowercase
 def iterate(soup):
 	
 	terms 		= []
-	definitions = []
+	definitions 	= []
 
 	for term in soup.find( 'div', "view-content clearfix" ).find_all( 'h2' ):
 		terms.append(term.text.encode( "utf8" ))
 
 	for definition in soup.find( 'div', "view-content clearfix" ).find_all( 'div', 'content clearfix' ):
-		s 		= ""
+		s = ""
 		for x in definition.find_all('p'):
 			s += ( x.text.encode( "utf8" ) )
 		definitions.append(s)
@@ -28,8 +28,8 @@ def print_contents(terms, definitions):
 for c in ascii_lowercase:
 	url	 					= 'https://www.economist.com/economics-a-to-z/'+c
 	r 						= requests.get( url )
-	data 					= r.text
-	soup 					= BeautifulSoup(data, "lxml")
-	terms, definitions		= iterate(soup)
+	data 						= r.text
+	soup 						= BeautifulSoup(data, "lxml")
+	terms, definitions				= iterate(soup)
 	print_contents(terms, definitions)
 	print '\n\n'
